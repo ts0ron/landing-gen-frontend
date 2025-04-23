a10b9243d2a2d17b040e055ecda603c0186b81cc
Ron Tsabari <rontsabari@Rons-MacBook-Pro.local>
2025-04-23 23:56:30 +0300

    SGA-004

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/landing-gen-frontend",
-  plugins: [react()],
+  base: "/landing-gen-frontend/",
+  plugins: [
+    react(),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        // 'fs',
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
